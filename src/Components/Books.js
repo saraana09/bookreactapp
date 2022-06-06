b064f6a5e3b5bf08a4cda62d0d76ab969d0dfa09
saraana09 <3.pengyou.000577@gmail.com>
2022-06-06 11:12:00 -0400

    side by side

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,33 +1,32 @@
 import React from "react";
 import { Card, Button, Container } from "react-bootstrap";
+import "../App.css";
 
 const Books = ({ books, dispatch }) => {
   return (
     <div className="fav">
       <h1>Google Book Results</h1>
       {books?.items?.map((item, index) => (
-        <Container>
-          <Card key={index}>
-            <Card.Img
-              src={
-                item.volumeInfo.imageLinks !== undefined
-                  ? item.volumeInfo.imageLinks.thumbnail
-                  : ""
-              }
-              alt={item.title}
-            />
-            <div>
-              <Card.Title key={item.id}>{item.volumeInfo.title}</Card.Title>
-              <Card.Body>{item.volumeInfo.authors}</Card.Body>
-            </div>
-            <Button
-              type="button"
-              onClick={() => dispatch({ type: "add_favorite", payload: item })}
-            >
-              Add to My Books
-            </Button>
-          </Card>
-        </Container>
+        <Card key={index}>
+          <Card.Img
+            src={
+              item.volumeInfo.imageLinks !== undefined
+                ? item.volumeInfo.imageLinks.thumbnail
+                : ""
+            }
+            alt={item.title}
+          />
+          <div>
+            <Card.Title key={item.id}>{item.volumeInfo.title}</Card.Title>
+            <Card.Body>{item.volumeInfo.authors}</Card.Body>
+          </div>
+          <Button
+            type="button"
+            onClick={() => dispatch({ type: "add_favorite", payload: item })}
+          >
+            Add to My Books
+          </Button>
+        </Card>
       ))}
     </div>
   );
